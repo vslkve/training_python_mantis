66135db04c3eb1f675f3e085b583d10d0aad92a6
Ekaterina V <kateotm@gmail.com>
2023-11-15 23:28:02 +0300

    Новое учебное приложение: баг-трекер Mantis

---
+++ fixture/session.py
@@ -0,0 +1,49 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+class SessionHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def login(self, username, password):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element(By.NAME, "username").click()
+        wd.find_element(By.NAME, "username").clear()
+        wd.find_element(By.NAME, "username").send_keys(username)
+        wd.find_element(By.NAME, "password").clear()
+        wd.find_element(By.NAME, "password").send_keys(password)
+        wd.find_element(By.XPATH, "//input[@value='Login']").click()
+
+    def ensure_login(self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
+
+    def logout(self):
+        wd = self.app.wd
+        wd.find_element(By.LINK_TEXT, "Logout").click()
+        time.sleep(1)
+
+    def ensure_logout(self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements(By.LINK_TEXT, "Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return self.get_logged_user() == username
+
+    def get_logged_user(self):
+        wd = self.app.wd
+        return wd.find_element(By.CSS_SELECTOR, "td.login-info-left span").text
