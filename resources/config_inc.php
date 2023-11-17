bf8f130fd86ace46efcd2a14cb3f25faebf684c8
Ekaterina V <kateotm@gmail.com>
2023-11-17 14:03:13 +0300

    Удалённое управление по протоколу Telnet

--- resources/config_inc.php
+++ resources/config_inc.php
@@ -5,3 +5,6 @@ $g_database_name = 'bugtracker';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_port = '25';
+$g_smtp_host = 'localhost';
